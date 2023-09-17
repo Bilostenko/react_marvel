c69d6ba0db59c1b06420a764a806cd10449b1e8a
Bilostenko <bilostenkod@gmail.com>
2023-09-17 10:50:51 +0300

    add charlist

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,7 @@
 class MarvelService {
 
   _apiBase = "https://gateway.marvel.com:443/v1/public/"
-  _apiKey = "4648eb02ef921529f9d259e4cb7fd213"
+  _apiKey = "apikey=4648eb02ef921529f9d259e4cb7fd213"
 
   getResource = async (url) => {
     let res = await fetch(url);
@@ -10,26 +10,27 @@ class MarvelService {
       throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
 
-    return await res.json()
+    return await res.json();
   }
 
-  getAllCharacters = () => {
-    return this.getResource(`${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`)
+  getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
   }
 
   getCharacter = async (id) => {
-    const res = await this.getResource(`${this._apiBase}characters/${id}?apikey=${this._apiKey}`)
-
-    return this._transformCharacter(res)
+    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0]);
   }
 
-  _transformCharacter = (res) => {
+  _transformCharacter = (char) => {
     return {
-      name: res.data.results[0].name,
-      description: res.data.results[0].description ? `${res.data.results[0].description.slice(0, 210)}...` : 'Ooops..no informatino for this character yet =(',
-      thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
-      homepage: res.data.results[0].urls[0].url,
-      wiki: res.data.results[0].urls[1].url
+      id: char.id,
+      name: char.name,
+      description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url
     }
   }
 }
